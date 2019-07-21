c72101c619142bb5fedc9824a1e2dc6e7406f25a
Takashi Kawasaki <espresso3389@gmail.com>
2019-07-21 22:06:39 +0900

    Update sample to add margin.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -26,7 +26,7 @@ class _MyAppState extends State<MyApp> {
   }
 
   Future initAsync() async {
-    await controller.initialize(width: 640, height: 480, fps: 30, cameraPosition: RtmpLiveViewCameraPosition.back);
+    await controller.initialize(width: 1280, height: 720, fps: 30, cameraPosition: RtmpLiveViewCameraPosition.back);
   }
 
   @override
@@ -50,22 +50,25 @@ class _MyAppState extends State<MyApp> {
             )
           ],
         ),
-        body: ValueListenableBuilder<RtmpStatus>(
-          valueListenable: controller.status,
-          builder: (context, status, child) => Column(
-            children: <Widget>[
-              AspectRatio(
-                aspectRatio: status?.aspectRatio ?? 1.0,
-                child: RtmpLiveView(controller: controller)
-              ),
-              SizedBox(height: 10),
-              Text(status?.isStreaming == true ? '${status.rtmpUrl} : ${status.streamName}' : 'Not connected.'),
-              Text(status != null ? 'Encoding: ${status.width} x ${status.height} ${status.fps} fps.' : ''),
-              Text(status != null ? 'Camera: ${status.cameraWidth} x ${status.cameraHeight}' : ''),
-            ]
-          )
-        ),
-      ),
+        body: Container(
+          margin: EdgeInsets.all(2),
+          child: ValueListenableBuilder<RtmpStatus>(
+            valueListenable: controller.status,
+            builder: (context, status, child) => Column(
+              children: <Widget>[
+                AspectRatio(
+                  aspectRatio: status?.aspectRatio ?? 1.0,
+                  child: RtmpLiveView(controller: controller)
+                ),
+                SizedBox(height: 10),
+                Text(status?.isStreaming == true ? '${status.rtmpUrl} : ${status.streamName}' : 'Not connected.'),
+                Text(status != null ? 'Encoding: ${status.width} x ${status.height} ${status.fps} fps.' : ''),
+                Text(status != null ? 'Camera: ${status.cameraWidth} x ${status.cameraHeight}' : ''),
+              ]
+            )
+          ),
+        )
+      )
     );
   }
 }
