05fc45c556c571c98992454b4dbfdc98e3885865
Takashi Kawasaki <espresso3389@gmail.com>
2019-07-24 06:48:54 +0900

    Android client could not preview/publish camera at the first time :(

--- android/src/main/kotlin/jp/espresso3389/flutter_rtmp_publisher/FlutterRtmpPublisherPlugin.kt
+++ android/src/main/kotlin/jp/espresso3389/flutter_rtmp_publisher/FlutterRtmpPublisherPlugin.kt
@@ -33,97 +33,100 @@ class FlutterRtmpPublisherPlugin(
   }
 
   override fun onMethodCall(call: MethodCall, result: Result) {
-    when {
-      call.method == "alloc" -> {
-        val tex = registrar.textures().createSurfaceTexture()
-        val textureId = tex.id()
-        val rtmpPub = RtmpPublisherWrapper(textureId, registrar, tex)
-        textures.put(textureId, rtmpPub)
-        result.success(textureId)
-      }
-      call.method == "release" -> {
-        val tex = call.argument<Number>("tex")!!.toLong()
-        val rtmpPub = textures[tex]
-        rtmpPub.release()
-        textures.delete(tex)
-        result.success(true)
-      }
-      call.method == "initCaptureConfig" -> {
-        PermissionRequester.requestPermissions(registrar, arrayOf(Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO)) {
-          grantAll ->
-          if (!grantAll) {
-            result.success(false)
-          } else {
-            val tex = call.argument<Number>("tex")!!.toLong()
-            val rtmpPub = textures[tex]
-            val width = call.argument<Number>("width")!!.toInt()
-            val height = call.argument<Number>("height")!!.toInt()
-            val fps = call.argument<Number>("fps")!!.toInt()
-            val camera = if (call.argument<String>("camera") == "back") CameraMode.BACK else CameraMode.FRONT
-            val audioBitRate = AUDIO_BITRATE
-            val videoBitRate = width * height * fps / BITRATE_MAGIC_DIVIDER
-            rtmpPub.setCaptureConfig(width, height, fps, camera, audioBitRate, videoBitRate)
-            result.success(true)
+    try {
+      when {
+        call.method == "alloc" -> {
+          PermissionRequester.requestPermissions(registrar, arrayOf(Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO)) { grantAll ->
+            if (!grantAll) {
+              result.error("nopermission", "Camera/audio access denied.", null);
+            } else {
+              val tex = registrar.textures().createSurfaceTexture()
+              val textureId = tex.id()
+              val rtmpPub = RtmpPublisherWrapper(textureId, registrar, tex)
+              textures.put(textureId, rtmpPub)
+              result.success(textureId)
+            }
           }
         }
-      }
-      call.method == "pause" -> {
-        val tex = call.argument<Number>("tex")!!.toLong()
-        val rtmpPub = textures[tex]
-        rtmpPub.pause()
-        result.success(true)
-      }
-      call.method == "resume" -> {
-        val tex = call.argument<Number>("tex")!!.toLong()
-        val rtmpPub = textures[tex]
-        rtmpPub.resume()
-        result.success(true)
-      }
-      call.method == "startPreview" -> {
-        val tex = call.argument<Number>("tex")!!.toLong()
-        val rtmpPub = textures[tex]
-        rtmpPub.onActivityResume()
-        result.success(true)
-      }
-      call.method == "stopPreview" -> {
-        val tex = call.argument<Number>("tex")!!.toLong()
-        val rtmpPub = textures[tex]
-        rtmpPub.onActivityPause()
-        result.success(true)
-      }
-      call.method == "connect" -> {
-        val tex = call.argument<Number>("tex")!!.toLong()
-        var rtmpUrl = call.argument<String>("url")
-        val name = call.argument<String>("name")
-
-        if (!rtmpUrl!!.endsWith('/')) {
-          rtmpUrl += "/"
+        call.method == "close" -> {
+          val tex = call.argument<Number>("tex")!!.toLong()
+          val rtmpPub = textures[tex]
+          rtmpPub.release()
+          textures.delete(tex)
+          result.success(true)
         }
-        // RTMP Publisher accepts one URL that contains stream name
-        rtmpUrl += name
+        call.method == "initCaptureConfig" -> {
+          val tex = call.argument<Number>("tex")!!.toLong()
+          val rtmpPub = textures[tex]
+          val width = call.argument<Number>("width")!!.toInt()
+          val height = call.argument<Number>("height")!!.toInt()
+          val fps = call.argument<Number>("fps")!!.toInt()
+          val camera = if (call.argument<String>("camera") == "back") CameraMode.BACK else CameraMode.FRONT
+          val audioBitRate = AUDIO_BITRATE
+          val videoBitRate = width * height * fps / BITRATE_MAGIC_DIVIDER
+          rtmpPub.setCaptureConfig(width, height, fps, camera, audioBitRate, videoBitRate)
+          result.success(true)
+        }
+        call.method == "pause" -> {
+          val tex = call.argument<Number>("tex")!!.toLong()
+          val rtmpPub = textures[tex]
+          rtmpPub.pause()
+          result.success(true)
+        }
+        call.method == "resume" -> {
+          val tex = call.argument<Number>("tex")!!.toLong()
+          val rtmpPub = textures[tex]
+          rtmpPub.resume()
+          result.success(true)
+        }
+        call.method == "startPreview" -> {
+          val tex = call.argument<Number>("tex")!!.toLong()
+          val rtmpPub = textures[tex]
+          rtmpPub.onActivityResume()
+          result.success(true)
+        }
+        call.method == "stopPreview" -> {
+          val tex = call.argument<Number>("tex")!!.toLong()
+          val rtmpPub = textures[tex]
+          rtmpPub.onActivityPause()
+          result.success(true)
+        }
+        call.method == "connect" -> {
+          val tex = call.argument<Number>("tex")!!.toLong()
+          var rtmpUrl = call.argument<String>("url")
+          val name = call.argument<String>("name")
 
-        val rtmpPub = textures[tex]
-        rtmpPub.connect(rtmpUrl!!)
-        result.success(true)
+          if (!rtmpUrl!!.endsWith('/')) {
+            rtmpUrl += "/"
+          }
+          // RTMP Publisher accepts one URL that contains stream name
+          rtmpUrl += name
+
+          val rtmpPub = textures[tex]
+          rtmpPub.connect(rtmpUrl!!)
+          result.success(true)
+        }
+        call.method == "disconnect" -> {
+          val tex = call.argument<Number>("tex")!!.toLong()
+          val rtmpPub = textures[tex]
+          rtmpPub.disconnect()
+          result.success(true)
+        }
+        call.method == "setCamera" -> {
+          val tex = call.argument<Number>("tex")!!.toLong()
+          val camera = if (call.argument<String>("camera") == "back") CameraMode.BACK else CameraMode.FRONT
+          val rtmpPub = textures[tex]
+          rtmpPub.setCameraMode(camera)
+          result.success(true)
+        }
+        call.method == "initFramework" -> {
+          // nothing for framework initialization
+          result.success(true)
+        }
+        else -> result.notImplemented()
       }
-      call.method == "disconnect" -> {
-        val tex = call.argument<Number>("tex")!!.toLong()
-        val rtmpPub = textures[tex]
-        rtmpPub.disconnect()
-        result.success(true)
-      }
-      call.method == "setCamera" -> {
-        val tex = call.argument<Number>("tex")!!.toLong()
-        val camera = if (call.argument<String>("camera") == "back") CameraMode.BACK else CameraMode.FRONT
-        val rtmpPub = textures[tex]
-        rtmpPub.setCameraMode(camera)
-        result.success(true)
-      }
-      call.method == "initFramework" -> {
-        // nothing for framework initialization
-        result.success(true)
-      }
-      else -> result.notImplemented()
+    } catch (e: Exception) {
+      result.error("exception", "Internal error.", e)
     }
   }
 
@@ -203,27 +206,27 @@ class FlutterRtmpPublisherPlugin(
     }
 
     override fun onConnected() {
-      eventSink!!.success("connected")
+      eventSink?.success("connected")
     }
 
     override fun onFailedToConnect() {
-      eventSink!!.success("failedToConnect")
+      eventSink?.success("failedToConnect")
     }
 
     override fun onPaused() {
-      eventSink!!.success("paused")
+      eventSink?.success("paused")
     }
 
     override fun onResumed() {
-      eventSink!!.success("resumed")
+      eventSink?.success("resumed")
     }
 
     override fun onDisconnected() {
-      eventSink!!.success("disconnected")
+      eventSink?.success("disconnected")
     }
 
     override fun onError(component: String?, e: Exception?) {
-      eventSink!!.success(hashMapOf(
+      eventSink?.success(hashMapOf(
         "name" to "error",
         "component" to component,
         "error" to e.toString()
